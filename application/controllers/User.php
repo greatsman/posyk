2640e1878562e5b59215f3e8ee48bd63d5abbbb1
Agung Septyanto Putra <agungseptyantoputr@gmail.com>
2020-05-28 23:38:54 +0900

    Add User Data + Form Validation Implementation

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -15,7 +15,31 @@ class User extends CI_Controller
 
     public function add()
     {
-        $this->template->load('template', 'user/add');
+        //memanggil library form validation
+        $this->load->library('form_validation');
+        //membuat rules untuk add user
+        $this->form_validation->set_rules('username', 'Username', 'required|min_length[5]|is_unique[user.username]');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules(
+            'passwordconf',
+            'Konfirmasi Password',
+            'required|matches[password]',
+            array(
+                'matches'      => '%s tidak sesuai dengan password',
+            )
+        );
+        $this->form_validation->set_rules('name', 'Nama', 'required');
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+        $this->form_validation->set_rules('level', 'Level', 'required');
+
+        $this->form_validation->set_message('required', 'Kolom %s Silahkan Diisi');
+        $this->form_validation->set_message('min_length', '{field} minimal 5 karakter');
+        $this->form_validation->set_message('is_unique', '{field} sudah dipakai, silhakan ganti');
+
+        if ($this->form_validation->run() ==  FALSE) {
+            $this->template->load('template', 'user/add');
+        } else {
+        }
     }
 }
         
--- application/views/user/add.php
+++ application/views/user/add.php
@@ -1,1 +1,73 @@
-Tambah Data
+<section class="content-header">
+    <h1>
+        User
+        <small>Pengguna</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">User</li>
+    </ol>
+</section>
+
+<!-- Main Content -->
+<section class="content">
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Add User</h3>
+            <div class="pull-right">
+                <a href="<?= site_url('user') ?>" class="btn btn-warning btn-flat">
+                    <i class="fa fa-undo"></i>
+                    Kembali
+                </a>
+            </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <form action="" method="post">
+                        <?= validation_errors() ?>
+                        <div class="form-group">
+                            <label for="">Username</label>
+                            <input type="text" name="username" value="<?= set_value('username') ?>" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Password Confirmation</label>
+                            <input type="password" name="passwordconf" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Nama</label>
+                            <input type="text" name="name" value="<?= set_value('name') ?>" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Alamat</label>
+                            <textarea name="address" id="" cols="30" rows="10" class="form-control">
+
+                            </textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Level</label>
+                            <select name="level" class="form-control">
+                                <option value="">--Pilih--</option>
+                                <option value="1">Admin</option>
+                                <option value="w">Kasir</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-success btn-flat">
+                                <i class="fa fa-paper-plane"></i>
+                                Simpan
+                            </button>
+                            <button type="reset" class="btn btn-flat"> Reset </button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
